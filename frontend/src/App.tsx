a36bf0ad24399158a1f8872ee9e6f7dce0b82151
Emiliano Russo <russo.emiliano97@gmail.com>
2024-04-13 20:06:34 -0300

    delete url

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,9 +3,9 @@ import React, { useState } from "react";
 function App() {
   const [url, setUrl] = useState("");
   const [shortUrl, setShortUrl] = useState("");
+  const [urlToDelete, setUrlToDelete] = useState("");
 
   const handleSubmit = async (e) => {
-    console.log("Acortando url...");
     e.preventDefault();
     const response = await fetch(`${process.env.REACT_APP_API_URL}/shorten`, {
       method: "POST",
@@ -16,6 +16,20 @@ function App() {
     });
     const data = await response.json();
     setShortUrl(data.shortUrl);
+  };
+
+  const handleDelete = async (e) => {
+    e.preventDefault();
+    const id = urlToDelete.split("/").pop(); // Extract ID from URL
+    const response = await fetch(`${process.env.REACT_APP_API_URL}/${id}`, {
+      method: "DELETE",
+    });
+    if (response.ok) {
+      alert("URL deleted successfully.");
+      setUrlToDelete("");
+    } else {
+      alert("Failed to delete URL.");
+    }
   };
 
   return (
@@ -47,6 +61,19 @@ function App() {
           </a>
         </p>
       )}
+
+      <form onSubmit={handleDelete}>
+        <input
+          type="text"
+          value={urlToDelete}
+          onChange={(e) => setUrlToDelete(e.target.value)}
+          placeholder="Enter URL to delete..."
+          required
+        />
+        <button style={{ background: "crimson" }} type="submit">
+          Delete URL
+        </button>
+      </form>
     </div>
   );
 }
